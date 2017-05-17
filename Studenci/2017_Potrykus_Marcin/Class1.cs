a933d3bf888483e5619439928da73ae7d07cd3c5
marcinl13 <marcinlpotrykus@gmail.com>
2017-05-17 15:49:02 +0200

    projekt v1

---
+++ Studenci/2017_Potrykus_Marcin/Class1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace obrotnica_v1a
+{
+    class Lista
+    {
+        public string wartosc;
+        public Lista next;
+
+        public int Dlugosc
+        {
+            get { return (next == null ? 1 : 1 + next.Dlugosc); }
+        }
+
+        public override string ToString()
+        {
+            if (next == null) return wartosc + "";
+            return wartosc + "|" + next.ToString();
+        }
+    }
+
+    class Lista2
+    {
+        public string[] wartosc;
+        public Lista2 next;
+
+        public int Dlugosc
+        {
+            get { return (next == null ? 1 : 1 + next.Dlugosc); }
+        }
+
+        public override string ToString()
+        {
+            if (next == null) return wartosc + "";
+            return wartosc + "|" + next.ToString();
+        }
+    }
+
+   /* public class Wagon
+    {
+        public int value;
+        public Wagon nextNode;
+
+        public Wagon(int v)
+        {
+            this.value = v;
+            this.nextNode = null;
+        }
+
+        public override string ToString()
+        {
+            if (nextNode == null)
+                return value + "";
+            return value + ", " + nextNode.ToString();
+        }
+    }*/
+
+    //public class Obrotnica
+    //{
+    //    public int valueo;
+    //    public Obrotnica nextNode;
+
+    //    public Obrotnica(int v)
+    //    {
+    //        this.valueo = v;
+    //        this.nextNode = null;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        if (nextNode == null)
+    //            return valueo + "";
+    //        return valueo + ", " + nextNode.ToString();
+    //    }
+    //}
+
+  /*  public class Wagon2
+    {
+        public Wagon head = null;
+        public Wagon tail = null;
+        public Wagon val;
+        //public Obrotnica head1 = null;
+        //public Obrotnica tail1 = null;
+
+        public void dodaj(Wagon node)
+        {
+            node.nextNode = head;
+            val = tail;
+            tail = head;
+            head = node;
+        }
+
+        public void usunGlowe()
+        {
+            if (head != null)
+            {
+                head = tail;
+                if (tail != null)
+                    tail = tail.nextNode;
+            }
+        }
+
+
+    }      */
+
+    public class Wagon
+    {
+        public string value;
+        public Wagon nextNode;
+
+        public Wagon(string v)
+        {
+            this.value = v;
+            this.nextNode = null;
+        }
+
+        public override string ToString()
+        {
+            if (nextNode == null)
+                return value + "";
+            return value + ", " + nextNode.ToString();
+        }
+    }
+
+    public class Lista3
+    {
+        public Wagon head = null;
+        public Wagon tail = null;
+        public Wagon val;
+        //public Obrotnica head1 = null;
+        //public Obrotnica tail1 = null;
+
+        public void dodaj(Wagon node)
+        {
+            node.nextNode = head;
+            val = tail;
+            tail = head;
+            head = node;
+        }
+
+        public void usunGlowe()
+        {
+            if (head != null)
+            {
+                head = tail;
+                if (tail != null)
+                    tail = tail.nextNode;
+            }
+        }
+
+
+        //public void dodaj_do_obrotnicy(Obrotnica obr)
+        //{
+        //    obr.nextNode = head1;
+        //    tail1 = head1;
+        //    head1 = obr;
+        //}
+
+        public override string ToString()
+        {
+            if (head == null) return "NULL";
+            return head.ToString();
+        }
+    }
+
+    public class Flaga
+    {
+        public int fl;
+    }
+
+    public class flaga2
+    {
+        public int flaga;
+
+        public flaga2 (int v)
+        {
+            this.flaga = v;            
+        }
+    }
+
+   /* struct SimpleStruct
+    {
+        private int xval;
+        private int min;
+        
+
+        public int X
+        {
+            get
+            {
+                return xval;
+            }
+            set
+            {
+                if (value < 100)                    xval = value;
+                else xval = 3;
+
+              
+
+
+            }
+        }
+        public void DisplayX()
+        {
+            Console.WriteLine("The stored value is: {0}", xval);
+        }
+    }*/
+
+
+}
--- Studenci/2017_Potrykus_Marcin/Program.cs
+++ Studenci/2017_Potrykus_Marcin/Program.cs
@@ -1,43 +1,212 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using obrotnica_v1a;
+using System.Text.RegularExpressions;
 
-namespace Algorymy_01
+
+namespace obrotnica_v1
 {
+    
     class Program
-    {
-        static int silnia(int n)
+    { 
+        static void WyświetlListe(Lista l)
         {
-            if (n > 1) return silnia(n - 1) * n;
-            else return 1 ;
+            Console.WriteLine("{0} ", l.wartosc);
+            if (l.next != null)
+            {
+                WyświetlListe(l.next);
+            }
         }
 
-        static string binarnie(int n)
+        static void DodajNaKońcu(Lista l, string wartosc)
         {
-            if (n > 0) return "" +binarnie(n / 2) + ""+ n%2;
-            else return "";
+            if (l.next != null)  DodajNaKońcu(l.next, wartosc);
+            
+            else
+            {
+                l.next = new Lista();
+                l.next.wartosc = wartosc;
+            }
+        }
+        
+        static void UsuńPrzód(ref Lista l)
+        {
+            l = l.next;
         }
 
-        static int suma(int n)
+        static void inicjacja(Lista dane, Lista tory,   Lista obrotnica, Lista rozkazy)
         {
-            if (n > 0 && n<1020) return n % 10 + suma(n/10);
-            else return 0;
+            int licz = 0;
+            try
+            {
+                string line;
+                StreamReader sr = new StreamReader("TextFile.txt");
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == "#")  licz++; 
+
+                    else
+                    {
+                        if      (licz == 0) DodajNaKońcu(dane, line);
+                        else if (licz == 1) 
+                        { 
+                            //Console.Write(line.Length);
+                            DodajNaKońcu(tory, line);                            
+
+                        }
+                        else if (licz == 2) DodajNaKońcu(obrotnica, line);
+                        else DodajNaKońcu(rozkazy, line);  
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR:");
+                Console.WriteLine(e.Message);
+            }
         }
 
+        static string[] SplitWords(string s)
+        {
+            return Regex.Split(s, @"\W+");
+        }
+
+        static void prawo(string liczba, Flaga f , int i, int ilosc_torów) 
+        {
+            int l = Convert.ToInt16(liczba);
+            int licz2 = l % ilosc_torów;
+            Console.WriteLine("licz2 " + licz2);
+            //if (licz2 >0)
+            //{
+                int licz = f.fl - l;
+                Console.WriteLine("prawo\t" + l + "flaga\t" + i + "licz\t" + licz);
+
+                if (licz < 0)
+                {
+                    int aa = ilosc_torów + licz;
+                    Console.WriteLine("FLAGA " + aa);
+                    f.fl = aa;
+                }
+            //}         
+        }
+
+        static void lewo(string liczba, Flaga f , int i, int ilosc_torów)
+        {
+            int l = Convert.ToInt16(liczba);
+            int licz2 = l % ilosc_torów;
+            Console.WriteLine("lewo\t" + l + " flaga\t" + i + " licz\t" + licz2);
+            if (licz2 > 1)
+            {
+                int licz = f.fl - licz2; //Console.WriteLine("licz " + licz);
+                if (licz < 0)
+                {                    
+                    int aa = 0 - licz;
+                    Console.WriteLine("FLAGA " + aa);
+                    f.fl = aa;
+                }
+            }
+            else 
+            {
+                int licz = f.fl - l; 
+                Console.WriteLine("licz  l2 " + licz);
+                if (licz < 0) 
+                {
+                    int aa = 0 - licz;
+                    Console.WriteLine("FLAGA " + aa);
+                    f.fl = aa;
+                }
+            }
+        }
+
+        static void pobierz(string liczba) 
+        { 
+            
+            Console.WriteLine("pobierz\t"+liczba); 
+        }
+
+        static void wydaj(string liczba) 
+        { Console.WriteLine("wydaj\t" + liczba); 
+        }
+
+        static void rozkazywanie(Lista r, int ilosc_torów)
+        {
+            Flaga flaga = new Flaga();
+            flaga.fl = 0;
+            Console.Write("flaga "+flaga.fl); 
+
+
+            int dlugosc=r.Dlugosc-1;
+            string [] symbol=new string[r.Dlugosc];
+            string [] liczba=new string[r.Dlugosc];
+            for (int i = 0; i < dlugosc;i++)
+            { 
+                string [] a = SplitWords(r.next.wartosc);
+                symbol[i] = a[0]; 
+                liczba[i] = a[1];
+                UsuńPrzód(ref r);
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < dlugosc;i++)
+            {
+                //rozkazuj
+                if (symbol[i] == "r") { prawo(liczba[i], flaga, flaga.fl, ilosc_torów); }
+                else if (symbol[i] == "l") { lewo(liczba[i],flaga, flaga.fl, ilosc_torów); }
+                else if (symbol[i] == "i") { pobierz(liczba[i]); }
+                else wydaj(liczba[i]);                
+            } Console.WriteLine("");
+        }
 
         static void Main(string[] args)
         {
-            int N = 5;
-            int N2 = 162;
-            Console.WriteLine("Silnia: {1} = {0}",silnia(N),N);
-            Console.WriteLine("Dziesietna na binarna: {1} =  {0}", binarnie(N),N);
-            int sum = suma(N2);
-            Console.WriteLine("Suma: {1} = {0}", sum, N2);
+            Lista dane = new Lista();
+            Lista tory = new Lista();
+            Lista obrotnica = new Lista();
+            Lista rozkazy = new Lista();
+
+           
             
-            Console.ReadLine();
+            inicjacja(dane, tory, obrotnica, rozkazy);            
+            
+            Console.WriteLine("dane\t\t<{0}>\t Dlugość<{1}>", dane.ToString(), dane.Dlugosc-1);
+            //Console.WriteLine("tory\t\t<{0}>\t Dlugość<{1}>", tory.ToString(), tory.Dlugosc-1);            
+            Console.WriteLine("obrotnica\t<{0}>\t Dlugość<{1}>", obrotnica.ToString(),obrotnica.Dlugosc-1);
+            //Console.WriteLine("rozkazy\t\t<{0}\t Dlugość<{1}>>", rozkazy.ToString(),rozkazy.Dlugosc-1);          
+            Console.WriteLine("\nROZKAZY");
+            
+
+            //SimpleStruct ss = new SimpleStruct();
+            //ss.X = 500;
+            //ss.DisplayX();
+             int ilosc_torów = Convert.ToInt16(dane.next.wartosc)-1;
+            Console.WriteLine("ilosc torów" + ilosc_torów);           
+            rozkazywanie(rozkazy, ilosc_torów); 
+             
+            Lista3[] toryw = new Lista3[10];           
+            //generuj tory
+            for (int ii = 0; ii < ilosc_torów; ii++)
+            {
+                toryw[ii] = new Lista3();
+                string[] ww = SplitWords(tory.next.wartosc);  
+                //generuj wagony
+                for (int w = 0; w < ww.Length; w++)
+                {
+                    toryw[ii].dodaj(new Wagon(ww[w]));
+                }
+                UsuńPrzód(ref tory);
+            }
+            //wypisz stan torów
+            for (int ii = 0; ii < ilosc_torów; ii++) { Console.WriteLine("Tor " + (ii+1) + "\t<{0}", toryw[ii]+">"); }
+            
+
+            
+
+
+
+
+           Console.ReadKey();
         }
     }
 }
