4c9307354e2ea0a0647cc385258258758d22d536
marcinl13 <marcinlpotrykus@gmail.com>
2017-05-22 10:13:10 +0200

    obrotnica beta

--- Studenci/2017_Potrykus_Marcin/Class1.cs
+++ Studenci/2017_Potrykus_Marcin/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Text;
 
@@ -21,89 +21,6 @@ namespace obrotnica_v1a
         }
     }
 
-    class Lista2
-    {
-        public string[] wartosc;
-        public Lista2 next;
-
-        public int Dlugosc
-        {
-            get { return (next == null ? 1 : 1 + next.Dlugosc); }
-        }
-
-        public override string ToString()
-        {
-            if (next == null) return wartosc + "";
-            return wartosc + "|" + next.ToString();
-        }
-    }
-
-   /* public class Wagon
-    {
-        public int value;
-        public Wagon nextNode;
-
-        public Wagon(int v)
-        {
-            this.value = v;
-            this.nextNode = null;
-        }
-
-        public override string ToString()
-        {
-            if (nextNode == null)
-                return value + "";
-            return value + ", " + nextNode.ToString();
-        }
-    }*/
-
-    //public class Obrotnica
-    //{
-    //    public int valueo;
-    //    public Obrotnica nextNode;
-
-    //    public Obrotnica(int v)
-    //    {
-    //        this.valueo = v;
-    //        this.nextNode = null;
-    //    }
-
-    //    public override string ToString()
-    //    {
-    //        if (nextNode == null)
-    //            return valueo + "";
-    //        return valueo + ", " + nextNode.ToString();
-    //    }
-    //}
-
-  /*  public class Wagon2
-    {
-        public Wagon head = null;
-        public Wagon tail = null;
-        public Wagon val;
-        //public Obrotnica head1 = null;
-        //public Obrotnica tail1 = null;
-
-        public void dodaj(Wagon node)
-        {
-            node.nextNode = head;
-            val = tail;
-            tail = head;
-            head = node;
-        }
-
-        public void usunGlowe()
-        {
-            if (head != null)
-            {
-                head = tail;
-                if (tail != null)
-                    tail = tail.nextNode;
-            }
-        }
-
-
-    }      */
 
     public class Wagon
     {
@@ -129,8 +46,7 @@ namespace obrotnica_v1a
         public Wagon head = null;
         public Wagon tail = null;
         public Wagon val;
-        //public Obrotnica head1 = null;
-        //public Obrotnica tail1 = null;
+        public int dlugosc;
 
         public void dodaj(Wagon node)
         {
@@ -138,10 +54,12 @@ namespace obrotnica_v1a
             val = tail;
             tail = head;
             head = node;
+            dlugosc += 1;
         }
 
         public void usunGlowe()
         {
+            dlugosc -= 1;
             if (head != null)
             {
                 head = tail;
@@ -149,14 +67,6 @@ namespace obrotnica_v1a
                     tail = tail.nextNode;
             }
         }
-
-
-        //public void dodaj_do_obrotnicy(Obrotnica obr)
-        //{
-        //    obr.nextNode = head1;
-        //    tail1 = head1;
-        //    head1 = obr;
-        //}
 
         public override string ToString()
         {
@@ -169,44 +79,5 @@ namespace obrotnica_v1a
     {
         public int fl;
     }
-
-    public class flaga2
-    {
-        public int flaga;
-
-        public flaga2 (int v)
-        {
-            this.flaga = v;            
-        }
-    }
-
-   /* struct SimpleStruct
-    {
-        private int xval;
-        private int min;
-        
-
-        public int X
-        {
-            get
-            {
-                return xval;
-            }
-            set
-            {
-                if (value < 100)                    xval = value;
-                else xval = 3;
-
-              
-
-
-            }
-        }
-        public void DisplayX()
-        {
-            Console.WriteLine("The stored value is: {0}", xval);
-        }
-    }*/
-
 
 }
